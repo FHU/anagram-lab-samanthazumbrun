2be1375e96f6c839e5d54f26847d6cae2408ece9
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-06 12:10:26 -0600

    firsttrymain.py
    sorted word/phrase in def and asked user for two inputs and print the def

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-#REMOVE PASS AND FIX THIS FUNCTION
+
 def anagram():
-    pass
+    if sorted(word1) == sorted(word2):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    word1 = input().lower().strip(' ')
+    word2 = input().lower().strip(' ')
+    print(anagram(word1, word2))
+
+
     
