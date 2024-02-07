78f338871edef70c3be56b100c11ec69322098c9
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-07 09:09:18 -0600

    main.py
    added whitespace= false

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-
 def anagram(word1, word2):
+    if word1.isspace():
+        return False
     if sorted(word1) == sorted(word2):
         return True
     else:
         return False
 
 if __name__ == '__main__':
-    word1 = input().lower().strip(" ")
-    word2 = input().lower().strip(" ")
+    word1 = input().lower()
+    word2 = input().lower()
     print(anagram(word1, word2))
 
 
