827837f5fb1f6400eaca71a5fd826feef89340f0
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-08 12:23:27 -0600

    main.py
    empty string check

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def anagram(word1, word2):
     word1 = word1.lower().replace(" ","")
     word2 = word2.lower().replace(" ","")
-    if word1.isspace():
+    if len(word1) == 0:
         return False
     if sorted(word1) == sorted(word2):
         return True
