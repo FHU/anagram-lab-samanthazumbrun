8ea784c569b571b9b14498a38de348e64d402668
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-08 12:20:03 -0600

    main.py
    moved lower and replace inside definition function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def anagram(word1, word2):
+    word1 = word1.lower().replace(" ","")
+    word2 = word2.lower().replace(" ","")
     if word1.isspace():
         return False
     if sorted(word1) == sorted(word2):
@@ -7,8 +9,8 @@ def anagram(word1, word2):
         return False
 
 if __name__ == '__main__':
-    word1 = input().lower().replace(" ","")
-    word2 = input().lower().replace(" ","")
+    word1 = input()
+    word2 = input()
     print(anagram(word1, word2))
     #test
 
